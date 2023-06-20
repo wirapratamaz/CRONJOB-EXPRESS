96ec4b76eab112eab048a7c9bfb40ddcb9e45fbb
wirapratamaz <wira@gringgo.co>
2023-06-20 16:55:20 +0800

    remove schedule remove file

--- src/cron.js
+++ src/cron.js
@@ -34,16 +34,16 @@ app.use((req, res, next) => {
 app.use('/', routes);
 
 //*fourth delete file
-const errorLogPath = path.join(__dirname, 'error.log');
+// const errorLogPath = path.join(__dirname, 'error.log');
 
-cron.schedule('0 20 * * * *', function() { //menit 20
-    console.log('---------------------');
-    console.log('Running Cron Job');
-    fs.unlink(errorLogPath, err => {
-      if (err) throw err;
-      console.log('Error file successfully deleted');
-    });
-});
+// cron.schedule('0 20 * * * *', function() { //menit 20
+//     console.log('---------------------');
+//     console.log('Running Cron Job');
+//     fs.unlink(errorLogPath, err => {
+//       if (err) throw err;
+//       console.log('Error file successfully deleted');
+//     });
+// });
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
